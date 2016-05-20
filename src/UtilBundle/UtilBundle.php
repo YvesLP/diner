9be4cc6c94fcb11b41b5360e331fe5e3e5fabeab
charlieLD <dussaultcharlie@gmail.com>
2016-05-20 09:38:05 +0200

    en cour

--- src/UtilBundle/UtilBundle.php
+++ src/UtilBundle/UtilBundle.php
@@ -1,9 +1,15 @@
 <?php
 
+// src/Acme/UserBundle/AcmeUserBundle.php
+
 namespace UtilBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UtilBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
